9219834d542441549ff160e36e65d1197a6757dd
Mike Raunsbæk <mike+git@raunsbaek.dk>
2014-02-26 15:39:21 +0100

    Added CPU usage and network RX and TX

--- index.php
+++ index.php
@@ -15,6 +15,42 @@ class ServerInfo
 	}
 
 
+	private static function cpupercent()
+	{
+		$cpu = exec('top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1}\'');
+
+
+		return array(
+			'percent' => $cpu
+		);
+	}
+
+
+	private static function network()
+	{
+		$interfaces = array();
+		exec('netstat -i | tail -n +3', $netstat);
+		$netstat = implode("\n", $netstat);
+		if ($netstat)
+		{
+			$lines = preg_split("/\n/", $netstat, -1, PREG_SPLIT_NO_EMPTY);
+			foreach($lines as $line)
+			{
+				$network = preg_split("/\s+/", $line);
+				$interfaces[] = $network['0'];
+			}
+
+			foreach($interfaces as $key => $interface)
+			{
+				$rx = exec('cat /sys/class/net/' . $interface . '/statistics/rx_bytes');
+				$tx = exec('cat /sys/class/net/' . $interface . '/statistics/tx_bytes');
+				$interfaces[$key] = array('name' => $interface, 'rxb' => $rx, 'txb' => $tx, 'rxp' => self::convertSize($rx, true), 'txp' => self::convertSize($tx, true));
+			}
+		}
+		return $interfaces;
+	}
+
+
 	private static function memory()
 	{
 		$results['ram'] = array('total' => 0, 'free' => 0, 'used' => 0, 'percent' => 0);
@@ -59,9 +95,11 @@ class ServerInfo
 	}
 
 
-	private static function convertSize($size)
+	private static function convertSize($size, $bytes = false)
 	{
-		$size = $size * 1024;
+		if($bytes == false)
+			$size = $size * 1024;
+
 		$filesizename = array('Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
 
 
@@ -91,6 +129,14 @@ class ServerInfo
 		$info['ram']['free'] = self::convertSize($memory['ram']['free']);
 		$info['ram']['used'] = self::convertSize($memory['ram']['used']);
 		$info['ram']['total'] = self::convertSize($memory['ram']['total']);
+
+
+		// CPU
+		$info['cpu'] = self::cpupercent();
+
+
+		// Network
+		$info['network'] = self::network();
 
 
 		// User infos
@@ -151,6 +197,17 @@ $info = ServerInfo::info();
 				<div class="col-sm-6">
 					<h4>Hukommelsesforbrug</h4>
 					<p><?php echo $info['ram']['percent'] . '% brugt - Fri ' . $info['ram']['free'] . ', brugt ' . $info['ram']['used'] . ', total ' . $info['ram']['total']; ?></p>
+				</div>
+			</div>
+			<br />
+			<div class="row">
+				<div class="col-sm-6">
+					<h4>CPU brugt</h4>
+					<p><?php echo $info['cpu']['percent']; ?>%</p>
+				</div>
+				<div class="col-sm-6">
+					<h4>Netværk</h4>
+					<p><?php foreach($info['network'] as $network) echo $network['name'] . ' - ' . 'Received: ' . $network['rxp'] . ' - ' . 'Transfered: ' . $network['txp'] . '<br />'; ?></p>
 				</div>
 			</div>
 			<hr />
